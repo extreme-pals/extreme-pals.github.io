782e079ebf7d2e611d4de7212b4fbd213c7a0cd0
Erodotos Terpizis <43606097+ErodotosTerpizis@users.noreply.github.com>
2019-03-04 15:40:58 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -26,15 +26,66 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
+//   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+// }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-  document.body.style.backgroundColor = "#00368D";
-}
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+//   document.getElementById("main").style.marginLeft= "0";
+//   document.body.style.backgroundColor = "#00368D";
+// }
 
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".scroll").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+        
+    } // End if
+  });
+});
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 30){
+            $(".topnav").css({"background-color":" #003b99"});   
+        }
+        else{
+            $(".topnav").css({"background-color":"#00368D"});
+        }
+
+    })
+})
+// change active status of home and about button
+$(document).ready(function(){
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if(scroll < 670){
+         $("a").removeClass('active');
+         $("#home").addClass('active');
+      }
+      else {
+         $("a").removeClass('active');
+         $("#about").addClass('active');
+      }
+    });
+ })
