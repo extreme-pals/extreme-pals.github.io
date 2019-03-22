05c0e16e3503e6297f25d37cb727dd7e670f07bf
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2019-03-22 18:03:54 +0000

    Reformat all code

--- script.js
+++ script.js
@@ -2,85 +2,86 @@
 var countDownDate = new Date("March 30, 2019 10:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-  // Get todays date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + " days : " + hours + " hours : "
-  + minutes + " minutes : " + seconds + " seconds";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + " days : " + hours + " hours : " +
+        minutes + " minutes : " + seconds + " seconds";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
 }, 1000);
 
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $(".scroll").on('click', function(event) {
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $(".scroll").on('click', function (event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-        
-    } // End if
-  });
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+
+        } // End if
+    });
 });
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 30){
-            $(".topnav").css({"background-color":" #003b99"});   
-        }
-        else{
-            $(".topnav").css({"background-color":"#00368D"});
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 30) {
+            $(".topnav").css({
+                "background-color": " #003b99"
+            });
+        } else {
+            $(".topnav").css({
+                "background-color": "#00368D"
+            });
         }
 
     })
 })
 
 // change active status of home and about button
-$(document).ready(function(){
+$(document).ready(function () {
     $(window).scroll(function (event) {
         var scroll = $(window).scrollTop();
-        if(scroll < 670){
-         $("a").removeClass('active');
-         $("#home").addClass('active');
-      }
-      else if(scroll < 1200) {
-         $("a").removeClass('active');
-         $("#about").addClass('active');
-      }
-      else {
-        $("a").removeClass('active');
-         $("#Contact").addClass('active');
-      }
-        
+        if (scroll < 670) {
+            $("a").removeClass('active');
+            $("#home").addClass('active');
+        } else if (scroll < 1200) {
+            $("a").removeClass('active');
+            $("#about").addClass('active');
+        } else {
+            $("a").removeClass('active');
+            $("#Contact").addClass('active');
+        }
+
     });
- })
+})
